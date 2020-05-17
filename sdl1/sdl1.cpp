a58310c756c71ea1d317f2aed1c002373f4104fd
ahmed <ahmed.malik8920@gmail.com>
2020-05-17 22:39:25 +0100

    quit game with exit key

--- sdl1/sdl1.cpp
+++ sdl1/sdl1.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
 	if (SDL_Init(SDL_INIT_VIDEO) < 0)
 		printf("Video Initialisation Error:", SDL_GetError());
 	else {
-		window = SDL_CreateWindow("Moba game test", NULL, NULL, ScreenWidth, ScreenHeight, SDL_WINDOW_SHOWN);
+		window = SDL_CreateWindow("Moba game test", 100, 100, ScreenWidth, ScreenHeight, SDL_WINDOW_SHOWN);
 		if (window == NULL)
 			printf("Window Creation Error:", SDL_GetError);
 		else {
@@ -20,13 +20,27 @@ int main(int argc, char* argv[])
 			if (imageSurface == NULL)
 				printf("Image loading failed: ", SDL_GetError());
 			else {
-				SDL_BlitSurface(imageSurface,NULL, windowSurface, NULL);
-				SDL_UpdateWindowSurface(window);
-				SDL_Delay(2000);
+				bool running = true;
+				SDL_Event ev;
+				while (running) {
+					while (SDL_PollEvent(&ev) != 0) {
+						if (ev.type == SDL_QUIT) {
+							running = false;
+							printf("Exiting game.");
+						}
+					}
+					SDL_BlitSurface(imageSurface, NULL, windowSurface, NULL);
+					SDL_UpdateWindowSurface(window);
+					SDL_Delay(2000);
+				}
 			}
 		}
 	}
+	SDL_FreeSurface(imageSurface);
+	imageSurface = nullptr;
 	SDL_DestroyWindow(window);
+	window = nullptr;
+	windowSurface = nullptr;
 	SDL_Quit();
 	return 0;
 }
